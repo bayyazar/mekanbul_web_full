b769c1700f528be65604d0e6753bb5a79232e4c5
bayyazar <noyinmahmut@gmail.com>
2025-12-17 16:07:32 +0300

    dfgfdg

--- frontend/src/components/AddComment.jsx
+++ frontend/src/components/AddComment.jsx
@@ -20,6 +20,10 @@ function AddComment() {
   
   // Sayfa yönlendirme işlemleri için kullanılır
   const navigate = useNavigate();
+  const handleModalClose = () => {
+    setShowModal (false);
+    navigate(`/venue/${id}`);
+  };
   
   // Form gönderildiğinde çalışan fonksiyon
   const onSubmit = (evt) => {
@@ -117,10 +121,7 @@ function AddComment() {
       />
     </>
   );
-  const handleModalClose = () => {
-    setShowModal (false);
-    navigate(`/venue/${id}`);
-  };
+  
 
 }
 
