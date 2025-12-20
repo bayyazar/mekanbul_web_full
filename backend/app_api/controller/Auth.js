14fc1e893a43bda926e95bdd40f00712e41cc37b
bayyazar <noyinmahmut@gmail.com>
2025-12-20 13:10:30 +0300

    fgsfsgfhsgs

--- backend/app_api/controller/Auth.js
+++ backend/app_api/controller/Auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
+const User = mongoose.model('user');
 
 const createResponse = (res, status, content) => {
     res.status(status).json(content);
