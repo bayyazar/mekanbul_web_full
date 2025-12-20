3fadf30dfdb32dbd90d9b475d0ab72ef792155ff
bayyazar <noyinmahmut@gmail.com>
2025-12-20 13:23:58 +0300

    gdhdhd

--- backend/app_api/config/passport.js
+++ backend/app_api/config/passport.js
@@ -4,17 +4,18 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 passport.use(
-    new LocalStrategy({
-    usernameField: "email",
-    passwordField: "password"
-    },
-    function(email, password, done) {
-        User.findOne({ email: email }).then((user) => {
-            if(!user.checkPassword(password)) {
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password"
+        },
+        function(email, password, done) {
+            User.findOne({ email: email }).then((user) => {
+                if(!user.checkPassword(password)) {
+                    return done(false);
+                }else {return done(user)};
+            }).catch((err) => {
                 return done(false);
-            }else {return done(user)};
-        }).catch((err) => {
-            return done(false);
-        });
-    }
+            });
+        }
 ));    
